8ae75160b97a692659df34daa0a9c2489f265bfe
Begimai <begimai.takeeva.ch@gmail.com>
2023-02-14 13:11:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import 'animate.css';
 
-function App() {
+
+const App = ()=>{
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 class="animate__animated animate__bounce ">An animated element</h1>
+
+      <div class="btn-group">
+        <button class="btn">Left</button>
+        <button class="btn">Middle</button>
+        <button class="btn">Right</button>
+      </div>
       </header>
     </div>
   );
